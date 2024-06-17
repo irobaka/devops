3329e57641d0d7e296045a7be1686316d6446168
Grzegorz Majta <grzegorz.majta@gmail.com>
2024-06-17 08:18:11 +0200

    fix restore

--- deployment/bin/restore.sh
+++ deployment/bin/restore.sh
@@ -3,12 +3,34 @@
 set -e
 
 BACKUP_FILENAME=$1
-POSTGRES_PASSWORD=$2
 
 PROJECT_DIR="/usr/src"
 BACKUP_DIR=$PROJECT_DIR"/storage/app/backup"
 
-docker compose exec app s3cmd cp s3://gmdevops/$BACKUP_FILENAME $PROJECT_DIR"/storage/app/backup.zip"
+cd /usr/src
+
+read_env() {
+  local filePath=".env"
+
+  if [ ! -f "$filePath" ]; then
+    echo "missing ${filePath}"
+    exit 1
+  fi
+
+  echo "Reading $filePath"
+  while read -r LINE; do
+    # Remove leading and trailing whitespaces, and carriage return
+    CLEANED_LINE=$(echo "$LINE" | awk '{$1=$1};1' | tr -d '\r')
+
+    if [[ $CLEANED_LINE != '#'* ]] && [[ $CLEANED_LINE == *'='* ]]; then
+      export "$CLEANED_LINE"
+    fi
+  done < "$filePath"
+}
+
+read_env
+
+docker compose exec app s3cmd --access_key=$AWS_ACCESS_KEY_ID secret_key=AWS_SECRET_ACCESS_KEY --region=AWS_DEFAULT_REGION --host=fra1.digitaloceanspaces.com --host-bucket=%(bucket)s.fra1.digitaloceanspaces.com cp s3://gmdevops/$BACKUP_FILENAME $PROJECT_DIR"/storage/app/backup.zip"
 
 docker compose exec app unzip -o $PROJECT_DIR"/storage/app/backup.zip" -d $BACKUP_DIR
 
@@ -16,7 +38,7 @@ docker compose exec app php $PROJECT_DIR"/artisan" down
 
 docker compose exec --user postgres postgres dropdb devops -f
 docker compose exec --user postgres postgres createdb devops
-docker compose exec -e PGPASSWORD=$POSTGRES_PASSWORD app psql -h postgres -U postgres -d devops < $BACKUP_DIR"/db-dumps/postgresql-devops.sql"
+docker compose exec -e PGPASSWORD=$DB_PASSWORD app psql -h postgres -U postgres -d devops < $BACKUP_DIR"/db-dumps/postgresql-devops.sql"
 
 docker compose exec app mv $PROJECT_DIR"/.env" $PROJECT_DIR"/.env_before_restore"
 docker compose exec app mv $PROJECT_DIR"/storage/app/public" $PROJECT_DIR"/storage/app/public_before_restore"
