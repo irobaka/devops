0d81a20af9c9a6c9a24d023d919395ba678c12b5
Grzegorz Majta <grzegorz.majta@gmail.com>
2024-06-17 22:06:12 +0200

    restore scripts

--- deployment/bin/restore.sh
+++ deployment/bin/restore.sh
@@ -30,23 +30,31 @@ read_env() {
 
 read_env
 
-docker compose exec app s3cmd --access_key=$AWS_ACCESS_KEY_ID secret_key=AWS_SECRET_ACCESS_KEY --region=AWS_DEFAULT_REGION --host=fra1.digitaloceanspaces.com --host-bucket=%(bucket)s.fra1.digitaloceanspaces.com cp s3://gmdevops/$BACKUP_FILENAME $PROJECT_DIR"/storage/app/backup.zip"
+docker compose exec app s3cmd --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY --region=$AWS_DEFAULT_REGION --host=fra1.digitaloceanspaces.com --host-bucket="%(bucket)s.fra1.digitaloceanspaces.com" get s3://gmdevops/$BACKUP_FILENAME $PROJECT_DIR"/storage/app/backup.zip"
 
 docker compose exec app unzip -o $PROJECT_DIR"/storage/app/backup.zip" -d $BACKUP_DIR
+docker compose exec app rm $PROJECT_DIR"/storage/app/backup.zip"
 
 docker compose exec app php $PROJECT_DIR"/artisan" down
 
 docker compose exec --user postgres postgres dropdb devops -f
 docker compose exec --user postgres postgres createdb devops
-docker compose exec -e PGPASSWORD=$DB_PASSWORD app psql -h postgres -U postgres -d devops < $BACKUP_DIR"/db-dumps/postgresql-devops.sql"
 
-docker compose exec app mv $PROJECT_DIR"/.env" $PROJECT_DIR"/.env_before_restore"
+docker compose exec -e PGPASSWORD=$DB_PASSWORD app pg_restore -h postgres -U postgres -d devops  $BACKUP_DIR/db-dumps/postgresql-devops.sql
+
+docker compose exec app cp $PROJECT_DIR"/.env" $PROJECT_DIR"/.env_before_restore"
 docker compose exec app mv $PROJECT_DIR"/storage/app/public" $PROJECT_DIR"/storage/app/public_before_restore"
 
-docker compose exec app mv $BACKUP_DIR/$PROJECT_DIR"/.env" $PROJECT_DIR"/.env"
+docker compose exec app cp $BACKUP_DIR/$PROJECT_DIR"/.env" $PROJECT_DIR"/.env"
 docker compose exec app mv $BACKUP_DIR/$PROJECT_DIR"/storage/app/public" $PROJECT_DIR"/storage/app/public"
 
-docker compose exec app php $PROJECT_DIR"/artisan"  storage:link
+docker compose exec app rm -rf $PROJECT_DIR"/storage/app/backup"
+
+docker compose exec app php $PROJECT_DIR"/artisan" storage:link
 docker compose exec app php $PROJECT_DIR"/artisan" optimize:clear
+docker compose exec app php $PROJECT_DIR"/artisan" config:cache
+docker compose exec app php $PROJECT_DIR"/artisan" route:cache
+docker compose exec app php $PROJECT_DIR"/artisan" view:cache
+docker compose exec app php $PROJECT_DIR"/artisan" event:cache
 
 docker compose exec app php $PROJECT_DIR"/artisan" up
--- deployment/bin/run_restore.sh
+++ deployment/bin/run_restore.sh
@@ -6,5 +6,5 @@ REMOTE_USER=$1
 SERVER_IP=$2
 BACKUP_FILENAME=$3
 
-scp -C -o StrictHostKeyChecking=no -i $HOME/.ssh/id_ed25519 ./restore.sh $REMOTE_USER@SERVER_IP:./restore.sh
+scp -C -o StrictHostKeyChecking=no -i $HOME/.ssh/id_ed25519 ./restore.sh $REMOTE_USER@$SERVER_IP:./restore.sh
 ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_ed25519 $REMOTE_USER@$SERVER_IP "chmod +x ./restore.sh && ./restore.sh $BACKUP_FILENAME"
