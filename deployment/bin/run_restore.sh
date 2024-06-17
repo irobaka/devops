ac607950069ca5fbd2c3130aba10fa6c66d53d57
Grzegorz Majta <grzegorz.majta@gmail.com>
2024-06-17 07:53:37 +0200

    add execute permission

--- deployment/bin/run_restore.sh
+++ deployment/bin/run_restore.sh
@@ -5,6 +5,7 @@ set -e
 REMOTE_USER=$1
 SERVER_IP=$2
 BACKUP_FILENAME=$3
+POSTGRES_PASSWORD=$4
 
-scp -C -o StrictHostKeyChecking=no -i $HOME/.ssh/id_ed25519 ./restore.sh $REMOTE_USER@SERVER_IP:./restore.sh
-ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_ed25519 $REMOTE_USER@$SERVER_IP "chmod +x ./restore.sh && ./restore.sh $BACKUP_FILENAME"
+scp -C -o StrictHostKeyChecking=no -i $HOME/.ssh/id_ed25519 ./restore.sh $REMOTE_USER@$SERVER_IP:./restore.sh
+ssh -tt -o StrictHostKeyChecking=no -i $HOME/.ssh/id_ed25519 $REMOTE_USER@$SERVER_IP "chmod +x ./restore.sh && ./restore.sh $BACKUP_FILENAME $POSTGRES_PASSWORD"
