3f1d7b3d53e260fd4056722cae54d126e11e8f32
Grzegorz Majta <grzegorz.majta@gmail.com>
2024-06-23 22:42:34 +0200

    Finał gitflow (#4)
    * chown prod files to devops user
    
    * remove pr workflow
    
    * fix provision demo server (#3)
    
    * fix provision demo server
    
    * fix provision demo server try 2
    
    * fix provision demo server try 3
    
    * fix provision demo server try 4
    
    * fix provision demo server try 5
    
    * fix provision demo server try 6
    
    * fix provision demo server try 7
    
    * fix provision demo server try 8
    
    * fix provision demo server try 9
    
    * fix provision demo server try 10
    
    * fix provision demo server try 11
    
    * fix provision demo server try 12

--- deployment/bin/run_provision_from_pipeline.sh
+++ deployment/bin/run_provision_from_pipeline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 NAME=$1
 DOCTL_TOKEN=$2
@@ -8,24 +8,20 @@ SSH_KEY_PRIVATE_LOCATION=$3
 SIZE=${4:-s-1vcpu-1gb}
 
 wget https://github.com/digitalocean/doctl/releases/download/v1.107.0/doctl-1.107.0-linux-amd64.tar.gz
-tar xf ./doctl-1.94.0-linux-amd64.tar.gz
+tar xf ./doctl-1.107.0-linux-amd64.tar.gz
 mv ./doctl /usr/local/bin
 
 EXISTING_DROPLET=$(doctl compute droplet list --access-token $DOCTL_TOKEN | grep $NAME)
 
-COUNT=${#EXISTING_DROPLET}
-
-if [ "$COUNT" -gt 0 ]; then
-  echo "Droplet already exists"
+if [ ${#EXISTING_DROPLET} -gt 0 ]; then
   SERVER_IP=$(echo $EXISTING_DROPLET | awk '{ print $3 }')
   echo $SERVER_IP
   exit 0
 fi
 
-
 SSH_FINGERPRINT=$(doctl compute ssh-key list --no-header --access-token $DOCTL_TOKEN | grep "legion5pro" | awk '{ print $3 }')
 
-DROPLET_ID=$(doctl compute droplet create $NAME --size $SIZE --image ubuntu-24-04-x64 --region fra1 --ssh-keys $SSH_FINGERPRINT --wait --no-header --format=ID --access-token $DOCTL_TOKEN)
+DROPLET_ID=$(doctl compute droplet create --size $SIZE --image ubuntu-24-04-x64 --region fra1 --ssh-keys $SSH_FINGERPRINT --wait --no-header --format=ID --access-token $DOCTL_TOKEN $NAME)
 
 #doctl projects resources assign 1dfbd682-6e4d-416d-8be1-8eec2f816cac --resource=do:droplet:$DROPLET_ID
 
