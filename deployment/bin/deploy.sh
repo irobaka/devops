63a982901da60c736a8803649973e7821fd0d9d4
Grzegorz Majta <grzegorz.majta@gmail.com>
2024-07-03 08:52:15 +0200

    split to multiple docker-compose files

--- deployment/bin/deploy.sh
+++ deployment/bin/deploy.sh
@@ -5,5 +5,5 @@ set -e
 cd /usr/src
 
 export $(cat .env)
-docker stack deploy -c docker-compose.prod.yml devops
+docker stack deploy -c docker-compose.yml devops
 #rm .env
--- deployment/bin/rollback.sh
+++ deployment/bin/rollback.sh
@@ -6,5 +6,5 @@ cd /usr/src
 b
 sed -i "/IMAGE_TAG/c\IMAGE_TAG=$IMAGE_TAG" /usr/src/.env
 
-sudo docker-compose -f docker-compose.prod.yml down
-sudo docker-compose -f docker-compose.prod.yml up -d --remove-orphans
+sudo docker-compose -f docker-compose.yml down
+sudo docker-compose -f docker-compose.yml up -d --remove-orphans
